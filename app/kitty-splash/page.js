b5f1b345d2d9d312df1504267d72ddb50ee20f6c
beam-ntth <beam4318.t@gmail.com>
2024-01-25 21:34:12 -0800

    initilized dunk game page

---
+++ app/kitty-splash/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const KittySplash = () => {
+    return (
+        <div>
+            <h1>Hello, Next.js!</h1>
+        </div>
+    );
+};
+
+export default KittySplash;
